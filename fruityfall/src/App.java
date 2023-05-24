8ca17c32d1b50699c34270e9f8e541bfb694203a
Lukas Lück <l.lueck.1@web.de>
2023-05-24 21:20:22 +0200

    added second side with back button

--- fruityfall/src/App.java
+++ fruityfall/src/App.java
@@ -10,9 +10,14 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
+    private Stage primaryStage;
+    private Scene mainScene;
+    private Scene newScene;
+
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("FruityFall v1.0");
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("FruityFall v1.0");
 
         // Fensterfarbe ändern
         StackPane root = new StackPane();
@@ -40,9 +45,33 @@ public class App extends Application {
         root.getChildren().add(vbox);
         StackPane.setAlignment(vbox, Pos.CENTER);
 
-        Scene scene = new Scene(root, 600, 500);
-        primaryStage.setScene(scene);
+        mainScene = new Scene(root, 600, 500);
+        primaryStage.setScene(mainScene);
         primaryStage.show();
+
+        // Ereignislistener für Button hinzufügen
+        button.setOnAction(e -> handleButtonClicked());
+    }
+
+    // Methode zum Behandeln des Button-Klicks
+    private void handleButtonClicked() {
+        // Neue Scene erstellen
+        StackPane newRoot = new StackPane();
+        newRoot.setStyle("-fx-background-color: orange;");
+        Button backButton = new Button("zurück zum Startbildschirm");
+        newRoot.getChildren().add(backButton);
+        StackPane.setAlignment(backButton, Pos.TOP_LEFT);
+
+        newScene = new Scene(newRoot, 600, 500);
+        primaryStage.setScene(newScene);
+
+        // Ereignislistener für Back-Button hinzufügen
+        backButton.setOnAction(e -> handleBackButtonClicked());
+    }
+
+    // Methode zum Behandeln des Back-Button-Klicks
+    private void handleBackButtonClicked() {
+        primaryStage.setScene(mainScene);
     }
 
     public static void main(String[] args) {
