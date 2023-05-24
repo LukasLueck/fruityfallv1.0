bb8a3ea9f3f142b67c50422212eaefa0ad8d8248
Lukas Lück <l.lueck.1@web.de>
2023-05-24 22:16:42 +0200

    added two text on second side

--- fruityfall/src/App.java
+++ fruityfall/src/App.java
@@ -2,13 +2,15 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -23,50 +25,68 @@ public class App extends Application {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("FruityFall v1.0");
 
-        // Fensterfarbe ändern
-        StackPane root = new StackPane();
-        root.setStyle("-fx-background-color: orange;");
-
         // VBox erstellen
         VBox vbox = new VBox();
         vbox.setAlignment(Pos.CENTER);
         vbox.setSpacing(10);
 
-        // Logo einfügen
+        // Bild einfügen
         Image image = new Image("fruityfall_logo.png");
         ImageView imageView = new ImageView(image);
         vbox.getChildren().add(imageView);
-        // Logo Bildgröße anpassen
+        // Bildgröße anpassen
         double newWidth = 400; // Neue Breite des Bildes
         double newHeight = 150; // Neue Höhe des Bildes
         imageView.setFitWidth(newWidth);
         imageView.setFitHeight(newHeight);
 
-        // Start-Button hinzufügen
+        // Button hinzufügen
         Button button = new Button("START THE GAME");
         vbox.getChildren().add(button);
 
-        root.getChildren().add(vbox);
-        StackPane.setAlignment(vbox, Pos.CENTER);
+        StackPane root = new StackPane(vbox);
+        root.setBackground(createBackground());
 
-        mainScene = new Scene(root, 600, 500);
+        mainScene = new Scene(root, 500, 500);
         primaryStage.setScene(mainScene);
         primaryStage.show();
 
         // Ereignislistener für Button hinzufügen
         button.setOnAction(e -> handleButtonClicked());
+
+        // Programm beenden Button hinzufügen
+        Button exitButton = new Button("SPIEL BEENDEN");
+        StackPane.setAlignment(exitButton, Pos.TOP_LEFT);
+        root.getChildren().add(exitButton);
+
+        // Ereignislistener für Exit-Button hinzufügen
+        exitButton.setOnAction(e -> handleExitButtonClicked());
     }
 
     // Methode zum Behandeln des Button-Klicks
     private void handleButtonClicked() {
+        
         // Neue Scene erstellen
         StackPane newRoot = new StackPane();
-        newRoot.setStyle("-fx-background-color: orange;");
+        newRoot.setBackground(createBackground());
+
+        // Eingabefelder hinzufügen
+        TextField textField1 = new TextField();
+        TextField textField2 = new TextField();
+
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(10);
+        vbox.getChildren().addAll(textField1, textField2);
+
+        newRoot.getChildren().add(vbox);
+        StackPane.setAlignment(vbox, Pos.CENTER);
+
         Button backButton = new Button("zurück zum Startbildschirm");
         newRoot.getChildren().add(backButton);
         StackPane.setAlignment(backButton, Pos.TOP_LEFT);
 
-        newScene = new Scene(newRoot, 600, 500);
+        newScene = new Scene(newRoot, 500, 500);
         primaryStage.setScene(newScene);
 
         // Ereignislistener für Back-Button hinzufügen
@@ -76,6 +96,19 @@ public class App extends Application {
     // Methode zum Behandeln des Back-Button-Klicks
     private void handleBackButtonClicked() {
         primaryStage.setScene(mainScene);
+    }
+
+    // Methode zum Erstellen des Hintergrundes
+    private Background createBackground() {
+        Image backgroundImage = new Image("background.png");
+        BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
+        BackgroundImage backgroundImg = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        return new Background(backgroundImg);
+    }
+
+    // Methode zum Behandeln des Exit-Button-Klicks
+    private void handleExitButtonClicked() {
+        primaryStage.close();
     }
 
     public static void main(String[] args) {
